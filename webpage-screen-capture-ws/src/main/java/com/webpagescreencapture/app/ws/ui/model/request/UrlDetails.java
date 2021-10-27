63eb0dd6d9f329a2b8fb51383663ff044f05b392
Narasimhan Mohan <38393650+NarasimhanMohan@users.noreply.github.com>
2021-10-27 13:17:39 +0530

    Initial commit

---
+++ webpage-screen-capture-ws/src/main/java/com/webpagescreencapture/app/ws/ui/model/request/UrlDetails.java
@@ -0,0 +1,17 @@
+package com.webpagescreencapture.app.ws.ui.model.request;
+
+import java.io.File;
+
+public class UrlDetails {
+
+	private String Url;
+
+	public String getUrl() {
+		return Url;
+	}
+
+	public void setUrl(String url) {
+		this.Url = url;
+	}
+	
+}
---
+++ webpage-screen-capture-ws/src/main/java/com/webpagescreencapture/app/ws/ui/model/response/UrlRest.java
@@ -0,0 +1,17 @@
+package com.webpagescreencapture.app.ws.ui.model.response;
+
+import java.io.File;
+
+public class UrlRest {
+
+	private String Url;
+
+	public String getUrl() {
+		return Url;
+	}
+
+	public void setUrl(String url) {
+		this.Url = url;
+	}
+	
+}
